3963b6304cf9b87e710b561f06751f1e938a757a
Alessandro Astone <ales.astone@gmail.com>
2025-07-18 14:59:50 +0000

    init: Substitute swiftshader with angle+pastel on vendor>=33
    Vendor 33 does not include a swiftshader OpenGL driver anymore;
    instead, the intended replacement is to use the swiftshader Vulkan
    driver called "pastel", with the ANGLE translator.
    
    Change-Id: Ibd998b56481992b641a34551ea05adf959c70f8f

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -1360,6 +1360,14 @@ void PropertyLoadBootDefaults() {
     load_properties_from_file("/odm_dlkm/etc/build.prop", nullptr, &properties);
     load_properties_from_partition("odm", /* support_legacy_path_until */ 28);
     load_properties_from_partition("product", /* support_legacy_path_until */ 30);
+ 
+    if (int vendor; ParseInt(properties["ro.vendor.build.version.sdk"], &vendor) &&
+        vendor >= 33) {
+        if (properties["ro.hardware.egl"] == "swiftshader") {
+            properties["ro.hardware.egl"] = "angle";
+            properties["ro.hardware.vulkan"] = "pastel";
+        }
+    }
 
     if (access(kDebugRamdiskProp, R_OK) == 0) {
         LOG(INFO) << "Loading " << kDebugRamdiskProp;
